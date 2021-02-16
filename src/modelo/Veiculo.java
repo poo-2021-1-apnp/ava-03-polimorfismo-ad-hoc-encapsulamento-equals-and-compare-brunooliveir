cd5cb4f54f8bfbfc6b3cc35fcf1969eab47417a2
brunooliveir <brunooliveir199@gmail.com>
2021-02-16 19:37:51 -0300

    codigo do objeto veiculo

--- src/modelo/Veiculo.java
+++ src/modelo/Veiculo.java
@@ -1,12 +1,43 @@
 package modelo;
 
-class Veiculo {
+public class Veiculo {
   final String placa, marca, modelo;
   final int    cilindrada;
 
-  Veiculo(String placa, String marca, String modelo, int cilindrada) {
+  public Veiculo(String placa, String marca, String modelo, int cilindrada) {
     this.placa = placa; this.marca = marca; this.modelo = modelo;
     this.cilindrada = cilindrada;
   }
+
+  public String getPlaca(){
+    return this.placa;
+  }
+
+  public String getModelo() {
+    return this.modelo;
+  }
+
+  public String getMarca() {
+    return this.marca;
+  }
+
+  public int cilindrada() {
+    return this.cilindrada;
+  }
+
+  @Override
+  public boolean equals(Object obj) { //criterio sera ser da mesma marca, modelo e cilindrada, nao sendo interessante a comparacao de placa
+    if (this == obj)
+      return true;
+    if (null == obj)
+      return false;
+    if (obj instanceof Veiculo) {
+      Veiculo outroDinheiro = (Veiculo) obj;
+      if (this.marca == outroDinheiro.marca && this.modelo == outroDinheiro.modelo && this.cilindrada == outroDinheiro.cilindrada)
+        return true;
+    }
+    return false;
+  }
+
 
 }
