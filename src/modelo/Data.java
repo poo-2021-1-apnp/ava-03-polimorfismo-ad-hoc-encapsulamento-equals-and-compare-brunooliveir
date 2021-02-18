66e6774a77f02de2064b5bd7c484fcc7c53962e8
brunooliveir <brunooliveir199@gmail.com>
2021-02-18 14:04:50 -0300

    Mudanças para tornar a leitura do código mais natual; isMesDe...() sets de proximo mes e ano, e sets de anterior dia, mes e ano.

--- src/modelo/Data.java
+++ src/modelo/Data.java
@@ -37,34 +37,69 @@ public class Data {
     return this.ano;
   }
 
+  public boolean isMesDe31Dias(){ // retorna true caso seja mês de 31 dias
+    return this.mes == 1 ||this.mes == 3 ||this.mes == 5 ||this.mes == 7 ||this.mes == 8 ||this.mes == 10|| this.mes == 12;
+  }
+
+  public boolean isMesDe30Dias() { // retorna true caso seja mês de 30 dias
+    return this.mes == 4 || this.mes == 6 || this.mes == 9 || this.mes == 11;
+  }
+
+  public boolean isMesDe28Dias() { // retorna true caso seja mês de 28 dias
+    return this.mes == 2;
+  }
+
+  public void setInicioProximoMes(){ // aumenta 1 mes e dia = 1
+    this.mes++;
+    this.dia = 1;
+  }
+
+  public void setFimMesPassado(){ // diminui 1 mes e dia = limite de dias do novo mes
+    int i = 0;
+    this.dia = 2;
+    this.mes--;
+    while (this.dia != 1) {
+      aumentaDia();
+      i++;
+    }
+    this.mes--;
+    this.dia = 1 + i;
+  }
+
+  public void setInicioProximoAno(){ // aumenta 1 ano, dia = 1 e mes = 1
+    this.mes = 1;
+    this.dia = 1;
+    this.ano++;
+  }
+
+  public void setFimAnoPassado(){ // diminui 1 ano, mes = 12 e dia = 31
+    this.ano--;
+    this.mes = MES_MAXIMO_12;
+    this.dia = DIA_MAXIMO_31;
+  }
 
   public void aumentaDia(){
     // aumenta o dia em 1,
     // caso ultrapasse valor maximo para o mes, retorna a 1 e aumenta 1 mes,
     // caso mes ultrapasse 12,
     // o ano é acrescido em +1
-    if (this.mes == 1 ||this.mes == 3 ||this.mes == 5 ||this.mes == 7 ||this.mes == 8 ||this.mes == 10|| this.mes == 12){
+    if (isMesDe31Dias()){
       if (this.dia == DIA_MAXIMO_31){
         if (this.mes == MES_MAXIMO_12){
-          this.mes = 1;
-          this.dia = 1;
-          this.ano++;
+          setInicioProximoAno();
         }else
-          this.mes++;
-          this.dia = 1;
+          setInicioProximoMes();
       }else
         this.dia +=1;
     }else{
-      if (this.mes == 2) {
+      if (isMesDe28Dias()) {
         if (this.dia == DIA_MAXIMO_28) {
-          this.mes++;
-          this.dia = 1;
+          setInicioProximoMes();
         }else
           this.dia++;
       }else{
         if (this.dia == DIA_MAXIMO_30){
-          this.mes++;
-          this.dia = 1;
+          setInicioProximoMes();
         }else
           this.dia++;
       }
@@ -94,26 +129,16 @@ public class Data {
     // se o mes originario for 1,
     // entao mes assume valor 12
     //e ano assume valor de (ano -1).
-    int i = 0;
     if (this.dia == 1){
-      if (this.mes == 1){
-        this.ano--;
-        this.mes = MES_MAXIMO_12;
-        this.dia = DIA_MAXIMO_31;
-      }
-      else{
-        this.dia = 2;
-        this.mes--;
-        while (this.dia != 1){
-          aumentaDia();
-          i++;
-        }
-        this.mes--;
-        this.dia = 1 + i;
-      }
-    }else{
+      if (this.mes == 1)
+        setFimAnoPassado();
+
+      else
+        setFimMesPassado();
+
+    }else
       this.dia--;
-    }
+
   }
 
   public void diminuiMes(){
@@ -131,64 +156,57 @@ public class Data {
 
   public void diminuiAno(){
     // e ano assume valor de (ano -1).
-    this.ano--;
+    if (this.ano > 0)
+      this.ano--;
   }
 
   public void setDia(int dia){  // define o dia com o valor inteiro informado
     if (dia < 1)
       throw new IllegalArgumentException("Dia invalido, menor que 1");
 
-    if (this.mes == 2){
+    if (isMesDe28Dias()){
       if (dia > DIA_MAXIMO_28)
         throw new IllegalArgumentException("Dia invalido, maior que 28 em fevereiro");
-
-      this.dia = dia;
     }
 
-    if (this.mes == 1 ||this.mes == 3 ||this.mes == 5 ||this.mes == 7 ||this.mes == 8 ||this.mes == 10 || this.mes == 12){
+    if (isMesDe31Dias()){
       if (dia > DIA_MAXIMO_31)
         throw new IllegalArgumentException("Dia invalido, maior que 31 no mes " + getMes());
-
-      this.dia = dia;
     }
-    if (this.mes == 4 || this.mes == 6 || this.mes == 9 || this.mes == 11){
+
+    if (isMesDe30Dias()){
       if (dia > DIA_MAXIMO_30)
         throw new IllegalArgumentException("Dia invalido, maior que 30 no mes " + getMes());
-
-      this.dia = dia;
     }
+
+    this.dia = dia;
   }
 
 
   public void setMes(int mes){  // define o mes com o valor inteiro informado
-    if (this.mes < 1)
+    if (mes < 1)
       throw new IllegalArgumentException("Mes invalido, menor que 1");
 
-    if (this.mes > MES_MAXIMO_12)
+    if (mes > MES_MAXIMO_12)
       throw new IllegalArgumentException("Mes invalido, maior que 12");
 
-    if (this.mes == 2){
-      if (this.dia > DIA_MAXIMO_28)
+    if (isMesDe28Dias()){
+      if (dia > DIA_MAXIMO_28)
         throw new IllegalArgumentException("Mes incompativel com o dia maior que 28, altere o dia primeiro");
-
-      this.mes = mes;
     }
 
-    if (this.mes == 4 || this.mes == 6 || this.mes == 9 || this.mes == 11){
+    if (isMesDe30Dias()){
       if (dia > DIA_MAXIMO_30)
         throw new IllegalArgumentException("Mes incompativel com o dia maior que 30, altere o dia primeiro");
-
-      this.mes = mes;
-
     }
 
-    if (this.mes == 1 || this.mes == 3 || this.mes == 5 || this.mes == 7 || this.mes == 8 || this.mes == 10 || this.mes == 12){
+    if (isMesDe31Dias()){
       if (dia > DIA_MAXIMO_31)
         throw new IllegalArgumentException("Mes incompativel com o dia maior que 31, altere o dia primeiro");
-
-      this.mes = mes;
-
     }
+
+    this.mes = mes;
+
   }
 
 
